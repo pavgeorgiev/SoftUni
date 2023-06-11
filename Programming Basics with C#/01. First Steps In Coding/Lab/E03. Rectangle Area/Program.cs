0ab65578ae1d4e14882f32b2ea6fc543108c8eff
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-11 23:59:41 +0300

    Solve "Rectangle Area" Lab

---
+++ Programming Basics with C#/01. First Steps In Coding/Lab/E03. Rectangle Area/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E03._Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
