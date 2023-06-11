a911e2bd182e7267e530876b58d22da44ade3ec7
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-11 23:38:59 +0300

    Solve "Hello SoftUni" Lab

---
+++ Programming Basics with C#/01. First Steps In Coding/Lab/E01. Hello SoftUni/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E01._Hello_SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
