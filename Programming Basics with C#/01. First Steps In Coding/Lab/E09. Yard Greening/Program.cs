1396dc79b50f75ce2348d5c122c473ecdb17f5d3
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-12 20:26:37 +0300

    Solve "Yard Greening" Lab

---
+++ Programming Basics with C#/01. First Steps In Coding/Lab/E09. Yard Greening/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E09._Yard_Greening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
