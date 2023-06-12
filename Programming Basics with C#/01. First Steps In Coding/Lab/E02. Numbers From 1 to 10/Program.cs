72a36e2f5666e741297b9d093b1a3d678ca1c9a8
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-12 22:26:15 +0300

    Update "Numbers From 1 to 10" Lab Solution

--- Programming Basics with C#/01. First Steps In Coding/Lab/E02. Numbers From 1 to 10/Program.cs
+++ Programming Basics with C#/01. First Steps In Coding/Lab/E02. Numbers From 1 to 10/Program.cs
@@ -2,11 +2,20 @@
 
 namespace E02._Numbers_From_1_to_10
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!"); // → 1 
+      Console.WriteLine(1); // → 1
+      Console.WriteLine(2); // → 2
+      Console.WriteLine(3); // → 3
+      Console.WriteLine(4); // → 4
+      Console.WriteLine(5); // → 5
+      Console.WriteLine(6); // → 6
+      Console.WriteLine(7); // → 7
+      Console.WriteLine(8); // → 8
+      Console.WriteLine(9); // → 9
+      Console.WriteLine(10); // → 10
     }
-    }
+  }
 }
