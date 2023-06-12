4fadaa543160524c05738d5842a2120045968ee8
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-12 22:13:04 +0300

    Update "Yard Greening" Lab Solution

--- Programming Basics with C#/01. First Steps In Coding/Lab/E09. Yard Greening/Program.cs
+++ Programming Basics with C#/01. First Steps In Coding/Lab/E09. Yard Greening/Program.cs
@@ -2,11 +2,20 @@
 
 namespace E09._Yard_Greening
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      double meters = double.Parse(Console.ReadLine());
+      double meterPrice = 7.61;
+      double discount = 0.18;
+
+      double metersPrice = meters * meterPrice;
+      double metersPriceDiscount = discount * metersPrice;
+      double finalPrice = metersPrice - metersPriceDiscount;
+
+      Console.WriteLine($"The final price is: {finalPrice:F2} lv."); // 550 → The final price is: 753.39 lv.
+      Console.WriteLine($"The discount is: {metersPriceDiscount:F2} lv."); // 550 → The discount is: 3432.11 lv.
     }
+  }
 }
