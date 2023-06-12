cbdaceca63d430813f01f52a855e34c575585c84
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-12 20:22:01 +0300

    Solve "Projects Creation" Lab

---
+++ Programming Basics with C#/01. First Steps In Coding/Lab/E07. Projects Creation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E07._Projects_Creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
