13dc46a4a530112ae9a2c0043b48b01ecd56ab00
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-12 22:12:05 +0300

    Update "Greeting by Name" Lab Solution

--- Programming Basics with C#/01. First Steps In Coding/Lab/E05. Greeting by Name/Program.cs
+++ Programming Basics with C#/01. First Steps In Coding/Lab/E05. Greeting by Name/Program.cs
@@ -2,15 +2,12 @@
 
 namespace E05._Greeting_by_Name
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-                    static void Main(string[] args)
-        {
-            string name = Console.ReadLine();
-            Console.WriteLine($"Hello, {name}!"); // Pavel → Hello, Pavel!
-        }
-        }
+      string name = Console.ReadLine();
+      Console.WriteLine($"Hello, {name}!"); // Pavel → Hello, Pavel!
     }
+  }
 }
