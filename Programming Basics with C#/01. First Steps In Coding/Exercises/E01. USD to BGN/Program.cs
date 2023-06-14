8596d93c6d762fc8679ec0386ea24adcdf4d5549
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-14 21:00:17 +0300

    Solve "USD to BGN" Exercise

---
+++ Programming Basics with C#/01. First Steps In Coding/Exercises/E01. USD to BGN/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace E01._USD_to_BGN
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double usd = double.Parse(Console.ReadLine());
+      double bgn = usd * 1.79549;
+
+      Console.WriteLine(bgn); // 3.1416 → 180.0004209182994
+    }
+  }
+}
