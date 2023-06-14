d1115c4bb6138d43dd05f66468b1de4e106e0b03
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-14 21:12:56 +0300

    Solve "Supplies for School" Exercise

---
+++ Programming Basics with C#/01. First Steps In Coding/Exercises/E05. Supplies for School/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace E05._Supplies_for_School
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double pensPrice = double.Parse(Console.ReadLine()) * 5.80;
+      double markersPrice = double.Parse(Console.ReadLine()) * 7.20;
+      double chalkboardCleanerPrice = double.Parse(Console.ReadLine()) * 1.20;
+      double discount = double.Parse(Console.ReadLine()) / 100;
+
+      double total = pensPrice + markersPrice + chalkboardCleanerPrice;
+      double discountedTotal = total - (total * discount);
+
+      Console.WriteLine(discountedTotal); // 2, 3, 4, 25 → 28.5
+    }
+  }
+}
