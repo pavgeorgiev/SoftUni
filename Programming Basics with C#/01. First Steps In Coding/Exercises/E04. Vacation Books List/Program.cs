5211082e8d83c791c9faf11be6a8a0ee28d36e95
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-14 21:11:19 +0300

    Solve "Vacation Books List" Exercise

---
+++ Programming Basics with C#/01. First Steps In Coding/Exercises/E04. Vacation Books List/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace E04._Vacation_Books_List
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int bookPages = int.Parse(Console.ReadLine());
+      int pagesPerHour = int.Parse(Console.ReadLine());
+      int numOfDays = int.Parse(Console.ReadLine());
+
+      int hoursPerDay = bookPages / pagesPerHour / numOfDays;
+      Console.WriteLine(hoursPerDay); // 212, 20, 2 → 5
+    }
+  }
+}
