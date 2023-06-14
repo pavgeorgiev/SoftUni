66afd8d5472b8199640c537e461a25883cd35dd9
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-14 21:04:04 +0300

    Solve "Radians to Degrees" Exercise

---
+++ Programming Basics with C#/01. First Steps In Coding/Exercises/E02. Radians to Degrees/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace E02._Radians_to_Degrees
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double radians = double.Parse(Console.ReadLine());
+      double degrees = radians * 180 / Math.PI;
+
+      Console.WriteLine(degrees); // 0.7854 → 45.00010522957485
+    }
+  }
+}
