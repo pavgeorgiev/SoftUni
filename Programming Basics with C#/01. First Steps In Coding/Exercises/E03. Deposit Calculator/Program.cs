804780fbd07b0a9c2d0d6c5e105be82b2d4be9ef
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-14 21:05:37 +0300

    Solve "Deposit Calculator" Exercise

---
+++ Programming Basics with C#/01. First Steps In Coding/Exercises/E03. Deposit Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace E03._Deposit_Calculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double depositAmount = double.Parse(Console.ReadLine());
+      int depositMonths = int.Parse(Console.ReadLine());
+      double interestRate = double.Parse(Console.ReadLine()) / 100;
+
+      double sum = depositAmount + depositMonths * ((depositAmount * interestRate) / 12);
+      Console.WriteLine(sum); // 200, 3, 5.7 → 202.85
+    }
+  }
+}
