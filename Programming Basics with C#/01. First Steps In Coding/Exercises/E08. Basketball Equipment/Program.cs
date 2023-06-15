5d6a85d84ce2a894d5e48b06da7ed419dc610518
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-15 09:57:53 +0300

    Solve "Basketball Equipment" Exercise

---
+++ Programming Basics with C#/01. First Steps In Coding/Exercises/E08. Basketball Equipment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace E08._Basketball_Equipment
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int annualFee = int.Parse(Console.ReadLine());
+      double shoesPrice = annualFee * 0.6;
+      double outfitPrice = shoesPrice * 0.8;
+      double ballPrice = outfitPrice / 4;
+      double accessoriesPrice = ballPrice / 5;
+
+      double totalPrice = annualFee + shoesPrice + outfitPrice + ballPrice + accessoriesPrice;
+      Console.WriteLine(totalPrice); // 365 → 811.76
+    }
+  }
+}
