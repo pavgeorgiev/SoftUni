066874eab1e1285a2fe9a5fcaf7f15769fa0b2ad
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-15 09:56:57 +0300

    Solve "Food Delivery" Exercise

---
+++ Programming Basics with C#/01. First Steps In Coding/Exercises/E07. Food Delivery/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace E07._Food_Delivery
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double chickenMenusPrice = double.Parse(Console.ReadLine()) * 10.35;
+      double fishMenusPrice = double.Parse(Console.ReadLine()) * 12.40;
+      double vegetarianMenusPrice = double.Parse(Console.ReadLine()) * 8.15;
+      double deliveryCost = 2.50;
+
+      double totalMenusPrice = chickenMenusPrice + fishMenusPrice + vegetarianMenusPrice;
+      double dessertPrice = totalMenusPrice * 0.2;
+      double totalCost = totalMenusPrice + dessertPrice + deliveryCost;
+
+      Console.WriteLine(totalCost); // 2, 4, 3 → 116.2
+    }
+  }
+}
