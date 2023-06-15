2f619b1fd6f4eb0a7386f9917b70a876ed662a3a
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-15 09:53:27 +0300

    Solve "Repainting" Exercise

---
+++ Programming Basics with C#/01. First Steps In Coding/Exercises/E06. Repainting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace E06._Repainting
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double nylonPrice = (double.Parse(Console.ReadLine()) + 2) * 1.5;
+      double paintPrice = (double.Parse(Console.ReadLine()) * 1.1) * 14.50;
+      double thinnerPrice = double.Parse(Console.ReadLine()) * 5;
+      double bagsPrice = 0.40;
+      int workCompletionHours = int.Parse(Console.ReadLine());
+
+      double totalMaterialsCost = nylonPrice + paintPrice + thinnerPrice + bagsPrice;
+      double workCompletedCost = (totalMaterialsCost * 0.3) * workCompletionHours;
+      double totalCost = totalMaterialsCost + workCompletedCost;
+
+      Console.WriteLine(totalCost); // 5, 10, 10, 1 → 727.09
+    }
+  }
+}
