3b38e9bac49d78382abe5ae44f7f856833f40eae
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-15 09:59:10 +0300

    Solve "Fish Tank" Exercise

---
+++ Programming Basics with C#/01. First Steps In Coding/Exercises/E09. Fish Tank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace E09._Fish_Tank
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int length = int.Parse(Console.ReadLine());
+      int width = int.Parse(Console.ReadLine());
+      int height = int.Parse(Console.ReadLine());
+      double occupiedPercentage = double.Parse(Console.ReadLine()) / 100;
+
+      double volume = length * width * height;
+      double cubicDecimeters = volume / 1000;
+
+
+      Console.WriteLine(cubicDecimeters * (1 - occupiedPercentage));
+      // 85, 75, 47, 17 → 248.68875
+    }
+  }
+}
