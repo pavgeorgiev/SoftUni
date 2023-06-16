263027e9699d071c25eb90d702a7568a18dbea78
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-16 12:51:54 +0300

    Solve "Excellent Result" Lab

---
+++ Programming Basics with C#/02. Conditional Statements/Lab/E01. Excellent Result/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace E01._Excellent_Result
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double grade = double.Parse(Console.ReadLine());
+
+      if (grade >= 5.50)
+      {
+        Console.WriteLine("Excellent!"); // 5.50 → Excellent!
+      }
+    }
+  }
+}
