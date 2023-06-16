469b1d820be091f729d44bdf26d9e6ed547f80ba
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-16 12:54:50 +0300

    Solve "Even or Odd" Lab

---
+++ Programming Basics with C#/02. Conditional Statements/Lab/E03. Even or Odd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace E03._Even_or_Odd
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int num = int.Parse(Console.ReadLine());
+
+      if (num % 2 == 0)
+      {
+        Console.WriteLine("even"); // 1024 → even
+      }
+      else
+      {
+        Console.WriteLine("odd");
+      }
+    }
+  }
+}
