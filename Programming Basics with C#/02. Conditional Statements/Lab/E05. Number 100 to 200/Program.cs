286156f561ff8559b990236c8196ca7bd43c74b3
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-17 22:47:32 +0300

    Solve "Number 100 to 200" Lab

---
+++ Programming Basics with C#/02. Conditional Statements/Lab/E05. Number 100 to 200/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace E05._Number_100_to_200
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int num = int.Parse(Console.ReadLine());
+
+      if (num < 100)
+      {
+        Console.WriteLine("Less than 100");
+      }
+      else if (num >= 100 && num <= 200)
+      {
+        Console.WriteLine("Between 100 and 200");
+      }
+      else
+      {
+        Console.WriteLine("Greater than 200"); // 210 → Greater than 200
+      }
+    }
+  }
+}
