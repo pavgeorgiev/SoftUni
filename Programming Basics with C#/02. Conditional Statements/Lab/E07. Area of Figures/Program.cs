cc76e8d78e7b32d227af93ba5734bbd5539f5650
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-17 22:52:02 +0300

    Solve "Area of Figures" Lab

---
+++ Programming Basics with C#/02. Conditional Statements/Lab/E07. Area of Figures/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace E07._Area_of_Figures
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string figureType = Console.ReadLine();
+      double area;
+
+      if (figureType == "square")
+      {
+        double a = double.Parse(Console.ReadLine());
+        area = a * a;
+      }
+      else if (figureType == "rectangle")
+      {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        area = a * b;
+      }
+      else if (figureType == "circle")
+      {
+        double a = double.Parse(Console.ReadLine());
+        area = a * a * Math.PI;
+      }
+      else
+      {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        area = (a * b) / 2;
+      }
+
+      Console.WriteLine($"{area:F3}");
+    }
+  }
+}
