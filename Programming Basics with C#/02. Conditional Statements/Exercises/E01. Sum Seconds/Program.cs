ec14fd235552a683e36136027ea480527d0135de
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-18 08:23:51 +0300

    Solve "Sum Seconds" Exercise

---
+++ Programming Basics with C#/02. Conditional Statements/Exercises/E01. Sum Seconds/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace E01._Sum_Seconds
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int firstTime = int.Parse(Console.ReadLine());
+      int secondTime = int.Parse(Console.ReadLine());
+      int thirdTime = int.Parse(Console.ReadLine());
+
+      int totalTime = firstTime + secondTime + thirdTime;
+      int seconds = totalTime % 60;
+      int minutes = totalTime / 60;
+
+      if (seconds < 10)
+      {
+        Console.WriteLine($"{minutes}:0{seconds}");
+      }
+      else
+      {
+        Console.WriteLine($"{minutes}:{seconds}"); // 50, 50, 49 → 2:29
+      }
+    }
+  }
+}
