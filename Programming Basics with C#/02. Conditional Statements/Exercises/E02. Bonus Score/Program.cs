60e1818c0a6b5d41d3fc93b2ef758c1a248f1c32
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-18 08:25:00 +0300

    Solve "Bonus Score" Exercise

---
+++ Programming Basics with C#/02. Conditional Statements/Exercises/E02. Bonus Score/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace E02._Bonus_Score
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int num = int.Parse(Console.ReadLine());
+      double bonusPoints = 0;
+
+      if (num > 1000)
+      {
+        bonusPoints += num * 0.1;
+      }
+      else if (num > 100)
+      {
+        bonusPoints += num * 0.2;
+      }
+      else
+      {
+        bonusPoints += 5;
+      }
+
+      if (num % 2 == 0)
+      {
+        bonusPoints++;
+      }
+
+      if (num % 10 == 5)
+      {
+        bonusPoints += 2;
+      }
+
+      Console.WriteLine(bonusPoints); // 175 → 37
+      Console.WriteLine(num + bonusPoints); // 175 → 212
+    }
+  }
+}
