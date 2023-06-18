acdd898fb1d3d7a742b9da3a3b6cda563307f45d
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-18 08:26:49 +0300

    Solve "Time Plus 15 Minutes" Exercise

---
+++ Programming Basics with C#/02. Conditional Statements/Exercises/E03. Time Plus 15 Minutes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace E03._Time_Plus_15_Minutes
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int hour = int.Parse(Console.ReadLine());
+      int minutes = int.Parse(Console.ReadLine());
+
+      int totalMinutes = (hour * 60) + minutes + 15;
+      int newHour = totalMinutes / 60 % 24;
+      int newMinutes = totalMinutes % 60;
+
+      Console.WriteLine($"{newHour}:{newMinutes:D2}"); // 23, 59 → 0:14
+    }
+  }
+}
