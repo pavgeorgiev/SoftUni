43f27ba40ef8740b402c4474e46d7450f1172904
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-19 17:45:26 +0300

    Solve "Shopping" Exercise

---
+++ Programming Basics with C#/02. Conditional Statements/Exercises/E07. Shopping/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace E07._Shopping
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double budget = double.Parse(Console.ReadLine());
+      int graphicsCardsCount = int.Parse(Console.ReadLine());
+      int processorsCount = int.Parse(Console.ReadLine());
+      int ramMemoriesCount = int.Parse(Console.ReadLine());
+
+      int graphicsCardPrice = 250;
+      double graphicsCardsPrice = graphicsCardPrice * graphicsCardsCount;
+
+      double processorPrice = graphicsCardsPrice * 0.35;
+      double processorsPrice = processorPrice * processorsCount;
+
+      double ramMemoryPrice = graphicsCardsPrice * 0.10;
+      double ramMemoriesPrice = ramMemoryPrice * ramMemoriesCount;
+
+      double totalSum = graphicsCardsPrice + processorsPrice + ramMemoriesPrice;
+
+      if (graphicsCardsCount > processorsCount)
+      {
+        totalSum *= 0.85;
+      }
+
+      double moneyLeft = budget - totalSum;
+
+      if (moneyLeft >= 0)
+      {
+        Console.WriteLine($"You have {moneyLeft:F2} leva left!");
+        // 900, 2, 1, 3 → You have 198.75 leva left!
+      }
+      else
+      {
+        Console.WriteLine($"Not enough money! You need {Math.Abs(moneyLeft):F2} leva more!");
+      }
+    }
+  }
+}
