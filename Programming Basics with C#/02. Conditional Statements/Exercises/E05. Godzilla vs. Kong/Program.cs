65a3fc7330061e94894391a136136ec15b30e8e6
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-19 10:07:40 +0300

    Solve "Godzilla vs. Kong" Exercise

---
+++ Programming Basics with C#/02. Conditional Statements/Exercises/E05. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace E05._Godzilla_vs._Kong
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double movieBudget = double.Parse(Console.ReadLine());
+      int extraCount = int.Parse(Console.ReadLine());
+      double extraClothingCost = double.Parse(Console.ReadLine());
+
+      double decorCost = movieBudget * 0.10;
+      double totalClothingCost = extraCount * extraClothingCost;
+
+      if (extraCount > 150)
+      {
+        totalClothingCost *= 0.90;
+      }
+
+      double movieCost = decorCost + totalClothingCost;
+      double remainingBudget = movieBudget - movieCost;
+
+      if (remainingBudget < 0)
+      {
+        Console.WriteLine("Not enough money!");
+        Console.WriteLine($"Wingard needs {Math.Abs(remainingBudget):F2} leva more.");
+      }
+      else
+      {
+        Console.WriteLine("Action!");
+        Console.WriteLine($"Wingard starts filming with {remainingBudget:F2} leva left.");
+        /* 20000, 120, 55.5 →
+           Action!
+           Wingard starts filming with 11340.00 leva left. */
+      }
+    }
+  }
+}
