3188a2d7f2a23b0ce335e43ae6ac166525024195
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-19 17:44:00 +0300

    Solve "World Swimming Record" Exercise

---
+++ Programming Basics with C#/02. Conditional Statements/Exercises/E06. World Swimming Record/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace E06._World_Swimming_Record
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double oldRecordInSeconds = double.Parse(Console.ReadLine());
+      double distanceInMeters = double.Parse(Console.ReadLine());
+      double timePerMeterInSeconds = double.Parse(Console.ReadLine());
+
+      double newRecordInSeconds = distanceInMeters * timePerMeterInSeconds;
+
+      double delayInSeconds = Math.Floor(distanceInMeters / 15.0) * 12.5;
+      newRecordInSeconds += delayInSeconds;
+
+      if (newRecordInSeconds < oldRecordInSeconds)
+      {
+        Console.WriteLine($"Yes, he succeeded! The new world record is {newRecordInSeconds:F2} seconds.");
+      }
+      else
+      {
+        double neededSeconds = newRecordInSeconds - oldRecordInSeconds;
+        Console.WriteLine($"No, he failed! He was {neededSeconds:F2} seconds slower.");
+        // 10464, 1500, 20 → No, he failed! He was 20786.00 seconds slower.
+      }
+    }
+  }
+}
