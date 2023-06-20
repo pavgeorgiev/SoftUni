8d54639a09cff7bbb10e5d61e0ffb68d23c40513
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-20 08:51:07 +0300

    Solve "Animal Type" Lab

---
+++ Programming Basics with C#/03. Conditional Statements Advanced/Lab/E03. Animal Type/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace E03._Animal_Type
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string animal = Console.ReadLine();
+      string animalType;
+
+      switch (animal)
+      {
+        case "dog":
+          animalType = "mammal";
+          break;
+        case "crocodile":
+        case "tortoise":
+        case "snake":
+          animalType = "reptile";
+          break;
+        default:
+          animalType = "unknown";
+          break;
+      }
+
+      Console.WriteLine(animalType); // snake → reptile
+    }
+  }
+}
