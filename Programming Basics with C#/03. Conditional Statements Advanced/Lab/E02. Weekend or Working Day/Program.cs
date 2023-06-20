6abfb9331d213fd74b71eab4bb670503d02ea73c
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-20 08:50:04 +0300

    Solve "Weekend or Working Day" Lab

---
+++ Programming Basics with C#/03. Conditional Statements Advanced/Lab/E02. Weekend or Working Day/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace E02._Weekend_or_Working_Day
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string dayOfTheWeek = Console.ReadLine();
+
+      switch (dayOfTheWeek)
+      {
+        case "Monday":
+        case "Tuesday":
+        case "Thursday":
+        case "Wednesday":
+        case "Friday":
+          Console.WriteLine("Working day");
+          break;
+        case "Saturday":
+        case "Sunday":
+          Console.WriteLine("Weekend"); // Sunday → Weekend
+          break;
+        default:
+          Console.WriteLine("Error");
+          break;
+      }
+    }
+  }
+}
