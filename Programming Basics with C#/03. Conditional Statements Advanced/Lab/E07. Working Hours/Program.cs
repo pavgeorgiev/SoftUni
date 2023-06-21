df388759545054c8e74d98fe1503d95f148de5be
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-21 07:42:56 +0300

    Solve "Working Hours" Lab

---
+++ Programming Basics with C#/03. Conditional Statements Advanced/Lab/E07. Working Hours/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace E07._Working_Hours
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int hourOfDay = int.Parse(Console.ReadLine());
+      string dayOfWeek = Console.ReadLine();
+
+      if (hourOfDay >= 10 && hourOfDay <= 18 && dayOfWeek != "Sunday")
+      {
+        Console.WriteLine("open"); // 11, Monday → open
+      }
+      else
+      {
+        Console.WriteLine("closed");
+      }
+    }
+  }
+}
