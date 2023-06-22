78573665130edea09a3f13a9a7b3b0851754f07b
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-22 13:16:38 +0300

    Solve "Invalid Number" Lab

---
+++ Programming Basics with C#/03. Conditional Statements Advanced/Lab/E10. Invalid Number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace E10._Invalid_Number
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int number = int.Parse(Console.ReadLine());
+
+      if (number != 0 && (number < 100 || number > 200))
+      {
+        Console.WriteLine("invalid"); // 75 → invalid
+      }
+    }
+  }
+}
