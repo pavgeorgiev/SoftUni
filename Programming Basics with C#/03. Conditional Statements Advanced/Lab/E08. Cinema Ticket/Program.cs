dbb842879cbd38f803af4a386dd6fe6a2366b693
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-22 13:14:07 +0300

    Solve "Cinema Ticket" Lab

---
+++ Programming Basics with C#/03. Conditional Statements Advanced/Lab/E08. Cinema Ticket/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace E08._Cinema_Ticket
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string dayOfWeek = Console.ReadLine();
+      int ticketPrice = 0;
+
+      switch (dayOfWeek)
+      {
+        case "Monday":
+          ticketPrice = 12;
+          break;
+        case "Tuesday":
+          ticketPrice = 12;
+          break;
+        case "Wednesday":
+          ticketPrice = 14;
+          break;
+        case "Thursday":
+          ticketPrice = 14;
+          break;
+        case "Friday":
+          ticketPrice = 12;
+          break;
+        case "Saturday":
+          ticketPrice = 16;
+          break;
+        case "Sunday":
+          ticketPrice = 16;
+          break;
+      }
+
+      Console.WriteLine(ticketPrice); // Friday → 12
+    }
+  }
+}
