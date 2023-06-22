51a4ceb7e633c95ed98d64c2bbb16a577819b0b5
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-22 13:15:08 +0300

    Solve "Fruit or Vegetable" Lab

---
+++ Programming Basics with C#/03. Conditional Statements Advanced/Lab/E09. Fruit or Vegetable/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace E09._Fruit_or_Vegetable
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string productName = Console.ReadLine();
+      string category;
+
+      switch (productName)
+      {
+        case "banana":
+        case "apple":
+        case "kiwi":
+        case "cherry":
+        case "lemon":
+        case "grapes":
+          category = "fruit";
+          break;
+        case "tomato":
+        case "cucumber":
+        case "pepper":
+        case "carrot":
+          category = "vegetable";
+          break;
+        default:
+          category = "unknown";
+          break;
+      }
+
+      Console.WriteLine(category); // apple → fruit
+    }
+  }
+}
