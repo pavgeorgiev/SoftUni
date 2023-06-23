889e12361cde08147c40f89bfe97e5f535794adf
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-23 10:12:45 +0300

    Solve "Trade Commissions" Lab

---
+++ Programming Basics with C#/03. Conditional Statements Advanced/Lab/E12. Trade Commissions/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace E12._Trade_Commissions
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string city = Console.ReadLine();
+      double sales = double.Parse(Console.ReadLine());
+      double commission = 0;
+      bool isValid = true;
+
+      if (sales >= 0 && sales <= 500)
+      {
+        switch (city)
+        {
+          case "Sofia":
+            commission = 0.05;
+            break;
+          case "Varna":
+            commission = 0.045;
+            break;
+          case "Plovdiv":
+            commission = 0.055;
+            break;
+          default:
+            isValid = false;
+            break;
+        }
+      }
+      else if (sales >= 500 && sales <= 1000)
+      {
+        switch (city)
+        {
+          case "Sofia":
+            commission = 0.07;
+            break;
+          case "Varna":
+            commission = 0.075;
+            break;
+          case "Plovdiv":
+            commission = 0.080;
+            break;
+          default:
+            isValid = false;
+            break;
+        }
+      }
+      else if (sales >= 1000 && sales <= 10000)
+      {
+        switch (city)
+        {
+          case "Sofia":
+            commission = 0.08;
+            break;
+          case "Varna":
+            commission = 0.10;
+            break;
+          case "Plovdiv":
+            commission = 0.12;
+            break;
+          default:
+            isValid = false;
+            break;
+        }
+      }
+      else if (sales > 10000)
+      {
+        switch (city)
+        {
+          case "Sofia":
+            commission = 0.12;
+            break;
+          case "Varna":
+            commission = 0.13;
+            break;
+          case "Plovdiv":
+            commission = 0.145;
+            break;
+          default:
+            isValid = false;
+            break;
+        }
+      }
+      else
+      {
+        isValid = false;
+      }
+
+      if (isValid)
+      {
+        Console.WriteLine("{0:F2}", sales * commission);
+      }
+      else
+      {
+        Console.WriteLine("error");
+      }
+    }
+  }
+}
