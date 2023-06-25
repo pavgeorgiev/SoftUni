1a2ff4682a093897a6c42d9bce9ac7cb24ffc4a2
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-25 13:21:07 +0300

    Solve "Journey" Exercise

---
+++ Programming Basics with C#/03. Conditional Statements Advanced/Exercises/E05. Journey/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace E05._Journey
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double budget = double.Parse(Console.ReadLine());
+      string season = Console.ReadLine();
+      string destination = "";
+      string accommodationType = "Hotel";
+      double accommodationCost = 0;
+
+      if (budget <= 100)
+      {
+        destination = "Bulgaria";
+        if (season == "summer")
+        {
+          accommodationType = "Camp";
+          accommodationCost = budget * 0.30;
+        }
+        else if (season == "winter")
+        {
+          accommodationCost = budget * 0.70;
+        }
+      }
+      else if (budget <= 1000)
+      {
+        destination = "Balkans";
+        if (season == "summer")
+        {
+          accommodationType = "Camp";
+          accommodationCost = budget * 0.40;
+        }
+        else if (season == "winter")
+        {
+          accommodationCost = budget * 0.80;
+        }
+      }
+      else if (budget >= 1000)
+      {
+        destination = "Europe";
+        accommodationCost = budget * 0.90;
+      }
+
+      Console.WriteLine($"Somewhere in {destination}");
+      Console.WriteLine($"{accommodationType} - {accommodationCost:F2}");
+    }
+  }
+}
