fb396e6ede4dc670b8a18cac4ea951594a3a5bb2
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-27 22:38:06 +0300

    Solve "Numbers from N to 1 in Reverse Order" Lab

---
+++ Programming Basics with C#/04. For Loop/Lab/E02. Numbers from N to 1 in Reverse Order/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace E02._Numbers_from_N_to_1_in_Reverse_Order
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int n = int.Parse(Console.ReadLine());
+
+      for (int i = n; i > 0; i--)
+      {
+        Console.WriteLine(i);
+      }
+    }
+  }
+}
