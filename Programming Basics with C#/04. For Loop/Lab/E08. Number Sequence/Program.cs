3aed2d3cb0321baa47aa1c3cbc91c23e8720b072
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-29 22:02:52 +0300

    Solve "Number Sequence" Lab

---
+++ Programming Basics with C#/04. For Loop/Lab/E08. Number Sequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace E08._Number_Sequence
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int n = int.Parse(Console.ReadLine());
+
+      int smallest = int.MaxValue;
+      int largest = int.MinValue;
+
+      for (int i = 0; i < n; i++)
+      {
+        int number = int.Parse(Console.ReadLine());
+        
+        if (number < smallest)
+        {
+          smallest = number;
+        }
+
+        if (number > largest)
+        {
+          largest = number;
+        }
+      }
+
+      Console.WriteLine("Max number: " + largest);
+      Console.WriteLine("Min number: " + smallest);
+    }
+  }
+}
