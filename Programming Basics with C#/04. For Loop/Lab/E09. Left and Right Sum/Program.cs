a53c0456a03c9151e11d31d6e8ff7deb3dec008c
Pavel Georgiev <pav.georgiev@outlook.com>
2023-06-30 16:40:03 +0300

    Solve "Left and Right Sum" Lab

---
+++ Programming Basics with C#/04. For Loop/Lab/E09. Left and Right Sum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace E09._Left_and_Right_Sum
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int n = int.Parse(Console.ReadLine());
+      int leftSum = 0;
+      int rightSum = 0;
+
+      for (int i = 0; i < n; i++)
+      {
+        leftSum += int.Parse(Console.ReadLine());
+      }
+
+      for (int i = 0; i < n; i++)
+      {
+        rightSum += int.Parse(Console.ReadLine());
+      }
+
+      int diff = leftSum - rightSum;
+
+      if (diff == 0)
+      {
+        Console.WriteLine($"Yes, sum = {leftSum}");
+      }
+      else
+      {
+        Console.WriteLine($"No, diff = {Math.Abs(diff)}");
+      }
+    }
+  }
+}
