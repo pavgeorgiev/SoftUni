885d3e6ee4fa90a060f22f7448c4c455f06e33a0
Pavel Georgiev <pav.georgiev@outlook.com>
2023-07-02 19:16:51 +0300

    Solve "Half Sum Element" Exercise

---
+++ Programming Basics with C#/04. For Loop/Exercises/E02. Half Sum Element/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace E02._Half_Sum_Element
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int n = int.Parse(Console.ReadLine());
+      int sum = 0;
+      int biggestNum = int.MinValue;
+
+      for (int i = 0; i < n; i++)
+      {
+        int num = int.Parse(Console.ReadLine());
+
+        if (num > biggestNum)
+        {
+          biggestNum = num;
+        }
+
+        sum += num;
+      }
+
+      int diff = (sum - biggestNum) - biggestNum;
+
+      if (diff == 0)
+      {
+        Console.WriteLine("Yes");
+        Console.WriteLine($"Sum = {biggestNum}");
+      }
+      else
+      {
+        Console.WriteLine("No");
+        Console.WriteLine($"Diff = {Math.Abs(diff)}");
+      }
+    }
+  }
+}
