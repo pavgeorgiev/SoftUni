7db1136b2a9d2ce857f1c7fbd55697fdcff16289
Pavel Georgiev <pav.georgiev@outlook.com>
2023-07-02 19:18:46 +0300

    Solve "Histogram" Exercise

---
+++ Programming Basics with C#/04. For Loop/Exercises/E03. Histogram/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace E03._Histogram
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int n = int.Parse(Console.ReadLine());
+      int p1Nums = 0;
+      int p2Nums = 0;
+      int p3Nums = 0;
+      int p4Nums = 0;
+      int p5Nums = 0;
+
+      for (int i = 0; i < n; i++)
+      {
+        int num = int.Parse(Console.ReadLine());
+
+        if (num < 200)
+        {
+          p1Nums++;
+        }
+        else if (num < 400)
+        {
+          p2Nums++;
+        }
+        else if (num < 600)
+        {
+          p3Nums++;
+        }
+        else if (num < 800)
+        {
+          p4Nums++;
+        }
+        else
+        {
+          p5Nums++;
+        }
+      }
+
+      Console.WriteLine($"{((double)p1Nums / n * 100):F2}%");
+      Console.WriteLine($"{((double)p2Nums / n * 100):F2}%");
+      Console.WriteLine($"{((double)p3Nums / n * 100):F2}%");
+      Console.WriteLine($"{((double)p4Nums / n * 100):F2}%");
+      Console.WriteLine($"{((double)p5Nums / n * 100):F2}%");
+    }
+  }
+}
