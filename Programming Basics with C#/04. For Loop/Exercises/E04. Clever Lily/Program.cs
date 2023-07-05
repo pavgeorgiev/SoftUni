239c74935907908089fbf00f2c9a08c6dbbaf8d7
Pavel Georgiev <pav.georgiev@outlook.com>
2023-07-05 08:37:22 +0300

    Solve "Clever Lily" Exercise

---
+++ Programming Basics with C#/04. For Loop/Exercises/E04. Clever Lily/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace E04._Clever_Lily
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int age = int.Parse(Console.ReadLine());
+      double washingMachinePrice = double.Parse(Console.ReadLine());
+      int toyPrice = int.Parse(Console.ReadLine());
+      double totalMoney = 0;
+      int moneyReceived = 10;
+
+      for (int i = 1; i <= age; i++)
+      {
+        if (i % 2 == 0)
+        {
+          totalMoney += moneyReceived - 1;
+          moneyReceived += 10;
+        }
+        else
+        {
+          totalMoney += toyPrice;
+        }
+      }
+
+      if (totalMoney < washingMachinePrice)
+      {
+        Console.WriteLine($"No! {washingMachinePrice - totalMoney:F2}");
+      }
+      else
+      {
+        Console.WriteLine($"Yes! {totalMoney - washingMachinePrice:F2}");
+      }
+    }
+  }
+}
