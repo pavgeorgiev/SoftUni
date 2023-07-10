deded7955850d14c4fdd25cd6c27f34e7395c6f1
Pavel Georgiev <pav.georgiev@outlook.com>
2023-07-10 22:56:19 +0300

    Solve "Min Number" Lab

---
+++ Programming Basics with C#/05. While Loop/Lab/E07. Min Number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace E07._Min_Number
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int smallestNumber = int.MaxValue;
+
+      string input = Console.ReadLine();
+      while (input != "Stop")
+      {
+        int num = int.Parse(input);
+
+        if (num < smallestNumber)
+        {
+          smallestNumber = num;
+        }
+
+        input = Console.ReadLine();
+      }
+
+      Console.WriteLine(smallestNumber);
+    }
+  }
+}
