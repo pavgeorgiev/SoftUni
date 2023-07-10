ef51a3d281aa96dc1c59c07bdba1d56459f65409
Pavel Georgiev <pav.georgiev@outlook.com>
2023-07-10 22:55:38 +0300

    Solve "Max Number" Lab

---
+++ Programming Basics with C#/05. While Loop/Lab/E06. Max Number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace E06._Max_Number
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int largestNumber = int.MinValue;
+
+      string input = Console.ReadLine();
+      while (input != "Stop")
+      {
+        int num = int.Parse(input);
+
+        if (num > largestNumber)
+        {
+          largestNumber = num;
+        }
+
+        input = Console.ReadLine();
+      }
+
+      Console.WriteLine(largestNumber);
+    }
+  }
+}
