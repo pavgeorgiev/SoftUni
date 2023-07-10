078be7aed7da0abbe8a2ef6870d17e56d290387b
Pavel Georgiev <pav.georgiev@outlook.com>
2023-07-10 22:54:48 +0300

    Solve "Account Balance" Lab

---
+++ Programming Basics with C#/05. While Loop/Lab/E05. Account Balance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace E05._Account_Balance
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double accountBalance = 0;
+
+      string input = Console.ReadLine();
+      while (input != "NoMoreMoney")
+      {
+        double num = double.Parse(input);
+
+        if (num < 0)
+        {
+          Console.WriteLine("Invalid operation!");
+          break;
+        }
+
+        accountBalance += num;
+        Console.WriteLine($"Increase: {num:F2}");
+        input = Console.ReadLine();
+      }
+
+      Console.WriteLine($"Total: {accountBalance:F2}");
+    }
+  }
+}
