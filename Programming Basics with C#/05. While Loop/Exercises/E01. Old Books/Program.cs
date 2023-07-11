d7c35c1f22300b3939fed3fd0c1a70229624e1b9
Pavel Georgiev <pav.georgiev@outlook.com>
2023-07-11 23:40:02 +0300

    Solve "Old Books" Exercise

---
+++ Programming Basics with C#/05. While Loop/Exercises/E01. Old Books/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace E01._Old_Books
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string favoriteBook = Console.ReadLine();
+      int numCheckedBooks = 0;
+
+      string book = Console.ReadLine();
+      while (book != "No More Books" && book != favoriteBook)
+      {
+        numCheckedBooks++;
+        book = Console.ReadLine();
+      }
+
+      if (book == favoriteBook)
+      {
+        Console.WriteLine($"You checked {numCheckedBooks} books and found it.");
+      }
+      else
+      {
+        Console.WriteLine("The book you search is not here!");
+        Console.WriteLine($"You checked {numCheckedBooks} books.");
+      }
+    }
+  }
+}
