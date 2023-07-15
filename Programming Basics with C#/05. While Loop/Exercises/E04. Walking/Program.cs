11f4289d79668c7a18771ad9321ea3eacc99c472
Pavel Georgiev <pav.georgiev@outlook.com>
2023-07-15 19:44:05 +0300

    Solve "Walking" Exercise

---
+++ Programming Basics with C#/05. While Loop/Exercises/E04. Walking/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace E04._Walking
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int goal = 10000;
+      int totalSteps = 0;
+
+      while (totalSteps < goal)
+      {
+        string input = Console.ReadLine();
+
+        if (input == "Going home")
+        {
+          int stepsOnWayHome = int.Parse(Console.ReadLine());
+          totalSteps += stepsOnWayHome;
+          break;
+        }
+
+        int steps = int.Parse(input);
+        totalSteps += steps;
+      }
+
+      if (totalSteps >= goal)
+      {
+        int difference = totalSteps - goal;
+        Console.WriteLine("Goal reached! Good job!");
+        Console.WriteLine($"{difference} steps over the goal!");
+      }
+
+      if (totalSteps < goal)
+      {
+        int difference = goal - totalSteps;
+        Console.WriteLine($"{difference} more steps to reach goal.");
+      }
+    }
+  }
+}
