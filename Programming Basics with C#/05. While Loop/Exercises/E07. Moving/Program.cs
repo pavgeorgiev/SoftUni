a7c904fbcaf165e575f4bbdf4949f173788e17d2
Pavel Georgiev <pav.georgiev@outlook.com>
2023-07-15 19:54:56 +0300

    Solve "Moving" Exercise

---
+++ Programming Basics with C#/05. While Loop/Exercises/E07. Moving/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace E07._Moving
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int width = int.Parse(Console.ReadLine());
+      int length = int.Parse(Console.ReadLine());
+      int height = int.Parse(Console.ReadLine());
+
+      int freeSpace = width * length * height;
+      int usedSpace = 0;
+
+      string input = Console.ReadLine();
+
+      while (input != "Done")
+      {
+        int boxes = int.Parse(input);
+        usedSpace += boxes;
+
+        if (usedSpace >= freeSpace)
+        {
+          int missingSpace = usedSpace - freeSpace;
+          Console.WriteLine($"No more free space! You need {missingSpace} Cubic meters more.");
+          return;
+        }
+
+        input = Console.ReadLine();
+      }
+
+      int remainingSpace = freeSpace - usedSpace;
+      Console.WriteLine($"{remainingSpace} Cubic meters left.");
+    }
+  }
+}
