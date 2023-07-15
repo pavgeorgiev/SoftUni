0e39aacb8da0db19c37d44efc82923316aab076e
Pavel Georgiev <pav.georgiev@outlook.com>
2023-07-15 19:47:26 +0300

    Solve "Cake" Exercise

---
+++ Programming Basics with C#/05. While Loop/Exercises/E06. Cake/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace E06._Cake
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int cakeWidth = int.Parse(Console.ReadLine());
+      int cakeLength = int.Parse(Console.ReadLine());
+      int cakeSize = cakeWidth * cakeLength;
+
+      while (cakeSize > 0)
+      {
+        string input = Console.ReadLine();
+
+        if (input == "STOP")
+        {
+          break;
+        }
+
+        int piecesTaken = int.Parse(input);
+        cakeSize -= piecesTaken;
+
+        if (cakeSize < 0)
+        {
+          break;
+        }
+      }
+
+      if (cakeSize >= 0)
+      {
+        Console.WriteLine($"{cakeSize} pieces are left.");
+      }
+
+      else
+      {
+        int piecesNeeded = Math.Abs(cakeSize);
+        Console.WriteLine($"No more cake left! You need {piecesNeeded} pieces more.");
+      }
+    }
+  }
+}
